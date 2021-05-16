143f2bee658fadbf207b8ec14d21a6b19d6d46c5
Marike <marike.puranen@gmail.com>
2021-05-16 11:09:32 +0300

    modify files

---
+++ PuzzleSolver/src/main/java/Solver/Main.java
@@ -0,0 +1,14 @@
+
+package Solver;
+
+import Solver.ui.SolverUi;
+
+/**
+ *
+ * @author marike
+ */
+public class Main {    
+    public static void main(String[] args) {
+        SolverUi.main(args);
+    }
+}
---
+++ PuzzleSolver/src/main/java/Solver/ui/SolverUi.java
@@ -0,0 +1,22 @@
+
+package Solver.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author marike
+ */
+public class SolverUi extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+    
+    public static void main(String args[]) {
+        launch(args);
+    }
+    
+}
