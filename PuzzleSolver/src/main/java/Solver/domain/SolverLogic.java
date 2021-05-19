6294f93b1bc3487a9a041f2ccd463326713254fb
Marike <marike.puranen@gmail.com>
2021-05-19 14:30:52 +0300

    satunnaisen ruudukon luominen

---
+++ PuzzleSolver/src/main/java/Solver/domain/SolverLogic.java
@@ -0,0 +1,54 @@
+
+package Solver.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Luokka sovelluslogiikan toteuttamiseen.
+ * @author marike
+ */
+public class SolverLogic {
+    
+    private int[][] ruudukko;
+    
+    /**
+     * Konstruktori alustaa ruudukon satunnaisilla numeroilla.
+     */
+    public SolverLogic() {
+        ruudukko = new int[4][4];
+        ruudukko = taytaRuudukko(ruudukko);
+    }
+    /**
+     * Täyttää ruudukon numeroilla 0-15 ja laittaa ne satunnaiseen järjestykseen.
+     * @param ruudukko Alustettu ruudukko
+     * @return täytetty ruudukko
+     */
+    public int[][] taytaRuudukko(int[][] ruudukko) {
+        ArrayList<Integer> numerot = new ArrayList<>();
+        
+        for (int i = 0; i <= 15; i++) {
+            numerot.add(i);
+        }
+        Collections.shuffle(numerot);
+        
+        int laskuri = 0;
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                ruudukko[i][j] = numerot.get(laskuri);
+                laskuri++;
+            }
+        }
+        return ruudukko;
+    }
+    
+    public void naytaRuudukko() {
+        // testi
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println(ruudukko[i][j]);
+            }
+        }
+    }
+}
--- PuzzleSolver/src/main/java/Solver/ui/SolverUi.java
+++ PuzzleSolver/src/main/java/Solver/ui/SolverUi.java
@@ -1,20 +1,26 @@
 
 package Solver.ui;
 
+import Solver.domain.SolverLogic;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 /**
- *
+ * Luokka graafisen käyttöliittymän totetuttamiseen.
  * @author marike
  */
 public class SolverUi extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+        // testi
+        SolverLogic solver = new SolverLogic();
+        solver.naytaRuudukko();
     }
-    
+    /**
+     * Main-metodi luokan käynnistämiseen.
+     * @param args 
+     */
     public static void main(String args[]) {
         launch(args);
     }
