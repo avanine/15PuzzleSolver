c289e7964eb9e040fbac65ce5daeed2d4d593d70
Marike <marike.puranen@gmail.com>
2021-05-19 22:05:57 +0300

    lisää koodia testeineen

--- src/main/java/solver/domain/SolverLogic.java
+++ src/main/java/solver/domain/SolverLogic.java
@@ -46,4 +46,53 @@ public class SolverLogic {
     public int[][] getRuudukko() {
         return ruudukko;
     }
+    /**
+     * Tarkistaa, onko peli mahdollista ratkaista.
+     * @return true tai false
+     */
+    public boolean onRatkeava() {
+        return true;
+    }
+    /**
+     * Laskee inversioiden lukumäärän, eli sen kuinka monta kertaa suurempi numero esiintyy ruudukossa ennen pienempää.
+     * @param ruudukko peliruudukko
+     * @return inversioiden lukumäärä
+     */
+    public int inversiot(int[][] ruudukko) {
+        int[] ruudukonNumerot = new int[16];
+        int indeksi = 0;
+        // muutetaan ruudukko arrayksi vertailua varten
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                ruudukonNumerot[indeksi] = ruudukko[i][j];
+                indeksi++;
+            }
+        }
+        
+        int laskuri = 0;
+        
+        for (int i = 0; i < 15; i++) {
+            for (int j = i + 1; j < 16; j++) {
+                if ((ruudukonNumerot[i] > ruudukonNumerot[j]) && (ruudukonNumerot[i] != 0) && (ruudukonNumerot[j] != 0)) {
+                    laskuri++;
+                }
+            }
+        }
+        return laskuri;
+    }
+    /**
+     * Palauttaa alhaalta laskettuna rivin, jolla tyhjä ruutu sijaitsee.
+     * @param ruudukko peliruudukko
+     * @return rivinumero
+     */
+    public int tyhjanRuudunSijainti(int[][] ruudukko) {
+        for (int i = 3; i >= 0; i--) {
+            for (int j = 3; j >= 0; j--) {
+                if (ruudukko[i][j] == 0) {
+                    return 4 - i;
+                }
+            }
+        }
+        return 0;
+    }
 }
--- src/test/java/domain/SolverLogicTest.java
+++ src/test/java/domain/SolverLogicTest.java
@@ -33,4 +33,33 @@ public class SolverLogicTest {
         }
         assertTrue(tyhjat == 1);
     }
+    
+    @Test
+    public void tyhjanRuudunSijaintiOikein() {
+        
+        int[][] testi = new int[4][4];
+        
+        int laskuri = 0;
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                testi[i][j] = laskuri;
+                laskuri++;
+            }
+        }
+        
+        assertTrue(solver.tyhjanRuudunSijainti(testi) == 4);
+        
+        testi[0][0] = 9;
+        testi[2][1] = 0;
+        
+        assertTrue(solver.tyhjanRuudunSijainti(testi) == 2);
+    }
+    
+    @Test
+    public void inversiotOikein() {
+        int[][] testi = { {1, 2, 3, 4} , {5, 6, 8, 7} , {9, 10, 11, 12} , {15, 14, 0, 13} };
+        
+        assertTrue(solver.inversiot(testi) == 4);
+    }
 }
